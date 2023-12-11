8282f6956a3bdf4d6c41993dbfe8f25c7e543b78
darksnowkun <33157460+Darksnowkun@users.noreply.github.com>
2023-12-11 10:13:26 -0600

    working version, just change prompt and num inference steps

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 from diffusers import StableDiffusionPipeline
+import torch
 import os
 from dotenv import load_dotenv
+
+from diffusers.pipelines.stable_diffusion import safety_checker
+def sc(self, clip_input, images) : return images, [False for i in images]
+# edit the StableDiffusionSafetyChecker class so that, when called, it just returns the images and an array of True values
+safety_checker.StableDiffusionSafetyChecker.forward = sc
 
 def setup():
     load_dotenv()
     token=os.getenv('HUG_TOKEN')
     return token
 
-url = ""
-pipeline = StableDiffusionPipeline.from_single_file(url)
 print(setup())
+
+url = "abyssorangemix3AOM3_aom3a1.safetensors"
+pipeline = StableDiffusionPipeline.from_single_file(url)
+from diffusers.pipelines.stable_diffusion import safety_checker
+def sc(self, clip_input, images) : return images, [False for i in images]
+# edit the StableDiffusionSafetyChecker class so that, when called, it just returns the images and an array of True values
+safety_checker.StableDiffusionSafetyChecker.forward = sc
+
+# Prompts
+my_prompt = "sfw, space, floating, stars, sun, moon, lunar, supernova, sfw"
+image = pipeline(prompt=my_prompt, num_inference_steps=10).images[0]
+from diffusers.pipelines.stable_diffusion import safety_checker
+def sc(self, clip_input, images) : return images, [False for i in images]
+# edit the StableDiffusionSafetyChecker class so that, when called, it just returns the images and an array of True values
+safety_checker.StableDiffusionSafetyChecker.forward = sc
+image.save("test13.png")
+image
